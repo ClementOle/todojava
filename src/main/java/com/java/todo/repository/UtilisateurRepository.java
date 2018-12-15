aba9cbee86d96efdfe318724c376e02c17e12856
ClementOle <clement.olewski@ipilyon.net>
2018-12-15 23:03:30 +0100

    Rectification d'erreur

--- src/main/java/com/java/todo/repository/UtilisateurRepository.java
+++ src/main/java/com/java/todo/repository/UtilisateurRepository.java
@@ -3,5 +3,9 @@ package com.java.todo.repository;
 import com.java.todo.model.Utilisateur;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
+
 public interface UtilisateurRepository extends CrudRepository<Utilisateur, Integer> {
+	List<Utilisateur> findAllByUsername(String username);
 }
