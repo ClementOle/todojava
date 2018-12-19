848babcc2d8f0469ad8957b57ae0a83937a9c39e
ClementOle <clement.olewski@ipilyon.net>
2018-12-19 17:52:45 +0100

    Rectification deleteUtilisateur

--- src/main/java/com/java/todo/controller/UtilisateurController.java
+++ src/main/java/com/java/todo/controller/UtilisateurController.java
@@ -3,10 +3,8 @@ package com.java.todo.controller;
 import com.java.todo.model.Utilisateur;
 import com.java.todo.service.UtilisateurService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UtilisateurController extends NullPointerException {
@@ -14,13 +12,13 @@ public class UtilisateurController extends NullPointerException {
 	private
 	UtilisateurService utilisateurService;
 
-	@RequestMapping(value = "/addUtilisateur", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
+	@RequestMapping(value = "", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
 	public Utilisateur addUtilisateur(@RequestBody Utilisateur utilisateur) {
 		return utilisateurService.addUser(utilisateur);
 	}
 
-	@RequestMapping(value = "/deleteUtilisateur", method = RequestMethod.DELETE)
-
+	@RequestMapping(value = "", method = RequestMethod.DELETE)
+	@ResponseStatus(value = HttpStatus.NO_CONTENT)
 	public void deleteUtilisateur(@RequestBody Utilisateur utilisateur) {
 		utilisateurService.deleteUser(utilisateur);
 	}
