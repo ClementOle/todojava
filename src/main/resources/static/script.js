ba97d1ac28d4f8a75d82321027e6244adb0f0077
ClementOle <clement.olewski@ipilyon.net>
2018-12-27 16:45:45 +0100

    Ajout de fichier pour tester les fonctionnalités

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,28 +1,105 @@
-const formInscription = document.getElementById('inscription');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
-const passwordVerif = document.getElementById('passwordVerif');
+const afficheEmploye = document.getElementById("afficheEmploye");
+const afficheListEmploye = document.getElementById("afficheListEmploye");
+
+const addUser = document.getElementById("addUser");
+const pseudo = document.getElementById("username");
+const mdp = document.getElementById("password");
+
+const deleteUser = document.getElementById("deleteUser");
+const idUtilisateur = document.getElementById("idUtilisateur");
+
+const idUser = document.getElementById("idUser");
+const listTask = document.getElementById("listTask");
+
+const addTask = document.getElementById("addTask");
+const idUtilisateurTask = document.getElementById("idUtilisateurTask");
+const textTask = document.getElementById("textTask");
 
 $(document).ready(function () {
-    formInscription.addEventListener('submit', function (event) {
-        event.stopPropagation();
+    afficheEmploye.addEventListener('click', function (event) {
         event.preventDefault();
-        let url = "http://localhost:8083/addUtilisateur?username=" + username.value + "&password=" + password.value + "&passwordVerif=" + passwordVerif.value;
+        event.stopPropagation();
+        let idUtilisateur = 22;
+        let url = "http://localhost:8083/" + idUtilisateur;
         $.get(url, function (data) {
-            switch (data) {
-                case 0:
-                    console.log("Compte créé !");
-                    break;
-                case 1:
-                    console.log("Non d'utilisateur déja utilisé");
-                    break;
-                case 2:
-                    console.log("Mot de passe non identiques");
-                    break;
-                default:
-                    console.log("Erreur serveur");
-                    break;
+            console.log(data);
+        });
+    });
+
+    afficheListEmploye.addEventListener('click', function (ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+
+
+        let numeroPage = 0;
+        let url = "http://localhost:8083?page=" + numeroPage;
+        $.get(url, function (data) {
+            console.log(data);
+        });
+    });
+
+    addUser.addEventListener('submit', function (ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+
+        let url = "http://localhost:8083";
+
+        let user = {
+            username: pseudo.value,
+            password: mdp.value
+        };
+
+        jQuery.ajax({
+            url: url,
+            type: "POST",
+            data: JSON.stringify(user),
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function (data) {
+                console.log(data);
             }
         });
     });
+
+    deleteUser.addEventListener('submit', function (ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+
+        let url = "http://localhost:8083/" + idUtilisateur.value;
+
+    });
+
+    listTask.addEventListener('submit', function (ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+
+        let url = "http://localhost:8083/" + idUser.value + "/tasks/?page=0";
+
+        $.get(url, function (data) {
+            console.log(data);
+        });
+    });
+
+    addTask.addEventListener('submit', function (ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+
+        let url = "http://localhost:8083/" + idUtilisateurTask.value + "/tasks/";
+
+        let task = {
+            text: textTask.value,
+            idUtilisateur: idUtilisateur.value
+        };
+
+        jQuery.ajax({
+            url: url,
+            type: "POST",
+            data: JSON.stringify(task),
+            dataType: "json",
+            contentType: "application/json; charset=utf-8",
+            success: function (data) {
+                console.log(data);
+            }
+        });
+    })
 });
